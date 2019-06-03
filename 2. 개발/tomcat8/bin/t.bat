994be30953fb1612e7041046b19b001f7a4ae613
cuh123 <chldnsgkr123@gmail.com>
2019-06-03 15:47:43 +0900

    톰캣 서버

---
+++ 2. 개발/tomcat8/bin/t.bat
@@ -0,0 +1,2 @@
+set classpath=.;C:\CUH\Web\tomcat8\lib\servlet-api.jar;C:\CUH\Web\tomcat8\lib\jsp-api.jar
+javac -d ../classes *.java
---
+++ 2. 개발/tomcat8/webapps/ROOT/WEB-INF/src/GetServlet.java
@@ -0,0 +1,11 @@
+package cuh.sv;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+class GetServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res){
+		
+	}
+}
