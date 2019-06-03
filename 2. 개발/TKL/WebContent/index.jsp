2fddef7383d4995056d9c0e63971852a8858f2eb
cuh123 <chldnsgkr123@gmail.com>
2019-06-03 14:56:22 +0900

    이클립스 서버 및 프로젝트 구성

---
+++ 2. 개발/TKL/WebContent/index.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html;charset=utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>First Project Index</title>
+</head>
+<body>
+	<center>
+		<h2>Take A Look</h2>
+		<br/>
+		<a href="product.do">상품</a>
+		<br/>
+	</center>
+</body>
+</html>
---
+++ 2. 개발/TKL/src/tkl/control/IndexControl.java
@@ -0,0 +1,18 @@
+package tkl.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index.do")
+public class IndexControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("index.jsp");
+	}
+
+}
